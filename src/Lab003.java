ab469eff230307f10a90d6ef9b59fd265813c750
Mdfaisal28 <faizaroaf@gmail.com>
2024-04-28 14:54:12 +0530

    AutomationJAVA

---
+++ src/Lab003.java
@@ -0,0 +1,20 @@
+package src;
+
+public class Lab003 {
+    public static void main(String[] args) {
+        //Keywords->Simple English words
+        //package,public,class-> Reserved words(Java)
+        //Blue colour Intellij
+        //Keywords are in Lowercase
+        //We can't use keywords as variables
+        byte age = 10;//Range of byte -128 to 127
+        age = 127;
+        System.out.println(age+1);
+        //age = age+1
+        System.out.println(age);
+
+
+    }
+
+
+    }
